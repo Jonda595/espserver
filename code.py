c0457e4ed28fda1ac142ee86d059a035ae080a2a
Jonda595 <jonas.vozka@outlook.cz>
2025-12-24 12:45:49 +0100

    Refactor code to set LED state directly
    Removed button polling loop and added initial LED state.

--- code.py
+++ code.py
@@ -1,15 +1,5 @@
 from machine import Pin
-import time
 
-but = Pin(5, Pin.IN, Pin.PULL_UP)
 led = Pin(15, Pin.OUT)
 
-while True:
-    val = but.value()
-    
-    if val == 1:
-        led.off()
-    elif val == 0:
-        led.on()
-    
-    time.sleep(0.1)
+led.on()
